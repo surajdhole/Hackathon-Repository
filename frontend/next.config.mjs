a9ff682ca692e2b7f810f363e940507f519d585c
Ciara Connolly <ciara.connolly@synechron.com>
2024-04-06 13:09:04 +0100

    Start of integrating with NextJs with Spring Boot backend. - incomplete

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/e3docs/:path*',
+          destination: 'http://localhost:8080/e3docs/:path*',
+        },
+      ]
+    },
+  }
+  export default nextConfig
+
+
+//   module.exports = nextConfig
